e280b603395a319beb5d47351b82fb0c73737362
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2025-01-31 21:04:03 +0700

    chore: missing docker building image and containerizing node and express env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World!!");
+import express from "express";
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Welcome to my app");
+});
+
+app.listen(8000, () => {
+  console.log("Server started on port 8000");
+});
