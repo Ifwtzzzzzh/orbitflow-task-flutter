71a207ae61c58e7adf8316aec5916a4463df723a
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2025-02-11 12:45:39 +0700

    style: styling home page ui

--- lib/core/constants/utils.dart
+++ lib/core/constants/utils.dart
@@ -6,3 +6,10 @@ Color strengthenColor(Color color, double factor) {
   int b = (color.blue * factor).clamp(0, 255).toInt();
   return Color.fromARGB(color.alpha, r, g, b);
 }
+
+List<DateTime> generateWeekDates(int weekOffset) {
+  final today = DateTime.now();
+  DateTime startOfWeek = today.subtract(Duration(days: today.weekday - 1));
+  startOfWeek = startOfWeek.add(Duration(days: weekOffset * 7));
+  return List.generate(7, (index) => startOfWeek.add(Duration(days: index)));
+}
